64a42168b0c642bf2de5f69dfd40255b42c4ce39
TerryKang <rkdxogns1234@daum.net>
2015-10-03 19:31:20 -0700

    Source file of Mimic
    This is the source file of brain game 'Mimic' during first term as a
    team project at BCIT

---
+++ Mimic/Source/config.php
@@ -0,0 +1,8 @@
+<?php
+    define('DB_HOST', 'mysql17.000webhost.com');
+    define('DB_USER', 'a4855499_group5');
+    define('DB_PASSWORD', 'milestone1536');
+    define('DB_DATABASE', 'a4855499_group5');
+    define('HOMEURL', 'index.php');
+?>
+
